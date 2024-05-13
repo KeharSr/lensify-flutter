f9a21108176909588fb6eac3b35943c64d878595
KeharSr <keharojha56@gmail.com>
2024-05-13 20:47:07 +0545

    Button Added

--- lib/screen/register_screen.dart
+++ lib/screen/register_screen.dart
@@ -6,72 +6,130 @@ class RegisterScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Register Screen'),
-        backgroundColor: Colors.blue[200],
-        centerTitle: true,
-      ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 200),
-            child: TextFormField(
-              decoration: InputDecoration(
-                hintText: 'Enter Your Email',
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              SizedBox(
+                height: 200,
+                child: Image.asset("assets/images/applogo.png"),
+              ),
+              const Text(
+                'Lets Create Your Account',
+                style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 20.0),
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.only(right: 8.0),
+                      child: Material(
+                        elevation: 10,
+                        child: TextFormField(
+                          decoration: const InputDecoration(
+                            labelText: 'First Name',
+                            border: OutlineInputBorder(),
+                            prefixIcon: Icon(Icons.person_3_sharp),
+                            labelStyle: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 8.0),
+                      child: Material(
+                        elevation: 10,
+                        child: TextFormField(
+                          decoration: const InputDecoration(
+                            labelText: 'Last Name',
+                            border: OutlineInputBorder(),
+                            prefixIcon: Icon(Icons.person),
+                            labelStyle: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 10.0),
+              Material(
+                elevation: 10,
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'Email',
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.email),
+                    labelStyle: TextStyle(fontWeight: FontWeight.bold),
+                  ),
                 ),
               ),
-            ),
+              const SizedBox(height: 10.0),
+              Material(
+                elevation: 10,
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'Number',
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.phone),
+                    labelStyle: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10.0),
+              Material(
+                elevation: 10,
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'Password',
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.lock),
+                    labelStyle: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  obscureText: true,
+                ),
+              ),
+              const SizedBox(height: 10.0),
+              Material(
+                elevation: 10,
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'Confirm Password',
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.lock),
+                    labelStyle: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  obscureText: true,
+                ),
+              ),
+              const SizedBox(height: 20.0),
+              TextButton(
+                onPressed: () {
+                  // Implement registration logic here
+                },
+                style: ButtonStyle(
+                  shape: const MaterialStatePropertyAll<OutlinedBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.zero,
+                    ),
+                  ),
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.greenAccent),
+                ),
+                child: const Text(
+                  'Create Account',
+                ),
+              ),
+            ],
           ),
-          Padding(
-            padding: const EdgeInsets.only(top: 15),
-            child: TextFormField(
-              decoration: InputDecoration(
-                hintText: 'Enter Your Phone Number',
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 15),
-            child: TextFormField(
-              obscureText: true,
-              decoration: InputDecoration(
-                hintText: 'Enter Your Password',
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 15),
-            child: TextFormField(
-              obscureText: true,
-              decoration: InputDecoration(
-                hintText: 'Confirm Your Password',
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 15),
-            child: ElevatedButton(
-              onPressed: () {},
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all<Color>(
-                  const Color.fromARGB(255, 212, 241, 213),
-                ),
-              ),
-              child: const Text('Register'),
-            ),
-          )
-        ],
+        ),
       ),
     );
   }
