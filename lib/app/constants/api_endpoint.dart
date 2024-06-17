98f207dc0e6eedb321736ffe74553461fe37105b
KeharSr <keharojha56@gmail.com>
2024-06-17 21:03:26 +0545

    api connection added

--- lib/app/constants/api_endpoint.dart
+++ lib/app/constants/api_endpoint.dart
@@ -1,4 +1,8 @@
 class ApiEndpoints{
-  
+  ApiEndpoints._();
+
+  static const Duration connectionTimeout = Duration(seconds: 1000);
+  static const Duration receiveTimeout = Duration(seconds: 1000);
+  static const String baseUrl = 'https://192.168.56.1:3000/api/v1/';
 
 }
