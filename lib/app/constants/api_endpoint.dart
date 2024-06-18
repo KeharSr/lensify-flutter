28adfd708486eee33910bcc7c1ef938487d58fb7
KeharSr <keharojha56@gmail.com>
2024-06-18 16:07:03 +0545

    register with api completed

--- lib/app/constants/api_endpoint.dart
+++ lib/app/constants/api_endpoint.dart
@@ -1,10 +1,10 @@
 class ApiEndpoints {
   ApiEndpoints._();
 
-  static const Duration connectionTimeout = Duration(seconds: 1000);
-  static const Duration receiveTimeout = Duration(seconds: 1000);
-  // static const String baseUrl = 'https:// 192.168.1.133:5000/api/';
-    static const String baseUrl = "http://10.0.2.2:5000/api/";
+  static const Duration connectionTimeout = Duration(seconds: 8000);
+  static const Duration receiveTimeout = Duration(seconds: 8000);
+  static const String baseUrl = 'http://192.168.1.133:5000/api/';
+  //   static const String baseUrl = "http://10.0.2.2:5000/api/";
 
 // ===========Auth Routes===========
   static const String loginUser = 'user/login';
