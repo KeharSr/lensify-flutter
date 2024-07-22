435e7a79ada033298a1e11d35cc2df2843115fa5
KeharSr <keharojha56@gmail.com>
2024-07-22 07:50:37 +0545

    repository created for cart

---
+++ lib/features/cart/data/repository/cart_remote_repository.dart
@@ -0,0 +1,22 @@
+import 'package:dartz/dartz.dart';
+import 'package:final_assignment/core/failure/failure.dart';
+import 'package:final_assignment/features/cart/data/data_source/remote/cart_remote_data_source.dart';
+import 'package:final_assignment/features/cart/domain/entity/cart_entity.dart';
+import 'package:final_assignment/features/cart/domain/repository/cart_repository.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final cartRemoteRepositoryProvider = Provider<CartRemoteRepository>((ref) {
+  final cartRemoteDataSource = ref.watch(cartRemoteDataSourceProvider);
+  return CartRemoteRepository(cartRemoteDataSource: cartRemoteDataSource);
+});
+
+class CartRemoteRepository implements ICartRepository{
+  final CartRemoteDataSource cartRemoteDataSource;
+
+  CartRemoteRepository({required this.cartRemoteDataSource});
+
+  @override
+  Future<Either<Failure, List<CartEntity>>> getCarts() {
+    return cartRemoteDataSource.getCarts();
+  }
+}
---
+++ lib/features/cart/domain/repository/cart_repository.dart
@@ -0,0 +1,13 @@
+import 'package:dartz/dartz.dart';
+import 'package:final_assignment/core/failure/failure.dart';
+import 'package:final_assignment/features/cart/data/repository/cart_remote_repository.dart';
+import 'package:final_assignment/features/cart/domain/entity/cart_entity.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final cartRepositoryProvider =
+ Provider<ICartRepository>((ref)=>ref.read (cartRemoteRepositoryProvider));
+
+abstract class ICartRepository {
+ 
+  Future<Either<Failure, List<CartEntity>>> getCarts();
+}
